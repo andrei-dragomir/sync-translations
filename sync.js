1dfcdeb5162ab8e38a6778a0eac7abecf6b59999
Andrei Dragomir <andu.tm@gmail.com>
2018-07-25 21:40:06 +0300

    First version of the sync script

---
+++ sync.js
@@ -0,0 +1,49 @@
+let _ = require('lodash');
+let fs = require('fs');
+
+function getTranslationJSON(lang) {
+    let langPath = '../zapf-connect/src/assets/i18n/'+lang+'.json';
+    return JSON.parse(fs.readFileSync(langPath, 'utf8'));
+}
+let baseLang = 'en_US';
+let availableLanguages = [
+    'de_DE',
+    // 'en_US',
+    'it',
+    'fr',
+    'nl',
+    'es',
+    'cs',
+    'da',
+    'pt'
+];
+
+let baseTranslation = getTranslationJSON(baseLang);
+
+
+function customizer(objValue, sourceValue, key) {
+    if(!key) {
+        console.log('\n\n', sourceValue);
+    }
+    if(typeof objValue == 'string') {
+        return objValue;
+    } else if(typeof sourceValue == 'string') {
+        return "";
+    }
+    return undefined;
+}
+
+availableLanguages.forEach((newLang) => {
+
+    let newTranslation = getTranslationJSON(newLang);
+    let mergedTranslation = _.mergeWith(newTranslation, baseTranslation, customizer);
+    let content = JSON.stringify(mergedTranslation, null, 4);
+
+    fs.writeFileSync('./'+newLang+'.json', content, 'utf-8');
+});
+
+
+
+
+
+
